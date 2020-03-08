ebe1dbb9448fc75ff2e947ed23a83a3290b5ca63
xwl <admin@0o0.me>
2020-03-08 16:29:39 +0800

    .

--- js/play.v3.js
+++ js/play.v3.js
@@ -14,18 +14,18 @@ $(function () {
       }
       result.url = result.url.replace("?rkey", new Date().getTime() + ".mp4?ver=6010&rkey").replace('https', 'http');
 
-      var ftn = result.url.substring(result.url.lastIndexOf("rkey=") + 5);
+      var ftn = result.url.substring(result.url.lastIndexOf("/"));
 
       var currentTime = localStorage.getItem("ct_" + pid);
 
       var index = 0;
       //eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('e f=[\'3://6-7.1.2.0\',\'3://g-7.1.2.0\',\'3://d-7.1.2.0\',\'3://9-4.1.2.0\',\'3://c-4.1.2.0\',\'3://6-4.1.2.0\',\'3://8-4.1.2.0\',\'3://a-4.1.2.0\',\'3://6-5.1.2.0\',\'3://6-b.1.2.0\',\'3://8-5.1.2.0\',\'3://8-b.1.2.0\',\'3://a-5.1.2.0\',\'3://9-5.1.2.0\'];',17,17,'com|ftn|qq|http|ctfs|btfs|sz|download|sh|tj|cd|btfsv2|xg|gzc|var|sites|njc'.split('|'),0,{}));
-      var sites=['\x74\x6a\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x78\x67\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x73\x7a\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x78\x61\x2d\x62\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x63\x64\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x73\x68\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x67\x7a\x63\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x6e\x6a\x63\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x73\x7a\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x74\x6a\x63\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d'];
+      var sites=['\x68\x74\x74\x70\x3a\x2f\x2f\x74\x6a\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x78\x67\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x73\x7a\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x78\x61\x2d\x62\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x63\x64\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x73\x68\x2d\x63\x74\x66\x73\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x67\x7a\x63\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x6e\x6a\x63\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d','\x68\x74\x74\x70\x3a\x2f\x2f\x73\x7a\x2d\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x2e\x66\x74\x6e\x2e\x71\x71\x2e\x63\x6f\x6d'];
       const dp = new DPlayer({
         container: document.getElementById('video'),
         autoplay: true,
         video: {
-          url: result.url.indexOf('bde4') > 0 ? result.url : "https://downproxy.weiyun.com/tx_tls_gate=" + sites[index++] + "/ftn_handler/" + ftn + "/" + new Date().getTime() + ".mp4",
+          url: result.url.indexOf('bde4') > 0 ? result.url : sites[index++] + ftn,
           pic: '/images/play_window_pic.png'
         }
       });
@@ -54,7 +54,7 @@ $(function () {
           url = result.url;
         }
         else
-          url = "https://downproxy.weiyun.com/tx_tls_gate=" + sites[index++] + "/ftn_handler/" + ftn + "/" + new Date().getTime() + ".mp4";
+          url = sites[index++] + ftn;
 
         dp.switchVideo({
           url: url.replace('https', 'http'),
@@ -80,7 +80,7 @@ $(function () {
       $('body').toast({
         position: 'top center',
         class: 'error',
-        message: '资源解析失败，请刷新重试！',
+        message: result.msg,
         showProgress: 'bottom'
       });
     }
@@ -90,10 +90,7 @@ $(function () {
     $('.player-wrapper .ui.sidebar')
       .sidebar({
         context: $('#player-box'),
-        transition: 'overlay',
-        onHide: function() {
-          clearTimeout(t);
-        }
+        transition: 'overlay'
       })
       .sidebar('toggle')
     ;
@@ -101,7 +98,7 @@ $(function () {
 
   toggle()
   $('#play-list').scrollTop($('#play-list .active').offset().top - $('#play-list').offset().top + $('#play-list').scrollTop());
-  var t = setTimeout(function () {
+  setTimeout(function () {
     toggle()
   }, 3000)
 })
