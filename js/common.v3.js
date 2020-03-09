d10e4d1135300236ba73405a0c824493c804e22e
xwl <admin@0o0.me>
2020-03-09 19:42:00 +0800

    .

--- js/common.v3.js
+++ js/common.v3.js
@@ -4,8 +4,35 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?70b86b24f83343ed8a495ada6dbc4357";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
-})();
 
+  var pageDATA_ua = window.navigator.userAgent.toLowerCase();
+
+  if(pageDATA_ua.indexOf('ucbrowser')>=0){setInterval("CaoNiMaDeUc();",1000);}
+})();
+function CaoNiMaDeUc(){
+  $("a").each(function(index, element) {
+    try{
+      var thishref=$(this).attr("href");
+      var thisText=$(this).html();
+      if(thishref.indexOf("uc.cn")>=0){
+        $(this).replaceWith(thisText);
+      }
+    }
+    catch(e){
+    }
+  });
+  $("script").each(function(index, element) {
+    try{
+      var thissrc=$(this).attr("src");
+
+      if(thissrc.indexOf("ucbrowser")>=0){
+        $(this).remove();
+      }
+    }
+    catch(e){
+    }
+  });
+}
 (function(){
   (function(){
     var canonicalURL, curProtocol;
